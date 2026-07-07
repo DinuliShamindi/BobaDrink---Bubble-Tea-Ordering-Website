36e13fcf7bd377bd5db1b154f15c3d6138072c19
Dinuli Wijesinghe <dinulishamindi@gmail.com>
2026-07-07 18:07:12 +0530

    fix github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: 'https://dinulishamindi.github.io/BobaDrink---Bubble-Tea-Ordering-Website/',
   plugins: [react()]
 });
